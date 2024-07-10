9204d234955224efafc14b35ea3c06a52e921822
AkRao47 <159660084+AkRao47@users.noreply.github.com>
2024-07-11 00:43:08 +0530

    minerfiic

--- uploadscript.py
+++ uploadscript.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = 1002183288516
+pyrogram.utils.MIN_CHANNEL_ID = -1002183288516
 import os
 
 # Replace these with your actual API credentials and bot token
